db23339dac55101381386bac80dd919cb8256e0e
JulianManera8 <julianmanera@hotmail.com>
2024-10-10 13:47:26 -0300

    ok

--- app/components/DeptoSelected.jsx
+++ app/components/DeptoSelected.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useLoaderData } from "@remix-run/react";
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import { Textarea } from "../components/ui/textarea";
 import { Checkbox } from "../components/ui/checkbox";
 import { Lock, LockOpen } from "lucide-react";
+import { useState } from "react";
 
 const departamentos = [
   {
@@ -166,6 +166,9 @@ export async function loader({ params }) {
 }
 
 export default function DeptoSelected({ slug }) {
+
+  const [edit, setEdit] = useState(false)
+
   const deptoSelected = departamentos.filter(
     (depto) => parseInt(slug) === depto.id
   );
@@ -195,182 +198,183 @@ export default function DeptoSelected({ slug }) {
         <h1 className="text-3xl text-gray-300 font-extrabold font-inter">
           DASHBOARD - Propiedad Seleccionada
         </h1>
-        <div className="flex gap-28 mr-6">
-          <span
-            className={`px-5 py-3 rounded-full text-xl font-semibold ${
-              deptoObjetoPrueba.estado === "Ocupado"
-                ? "bg-green-100 text-green-800 border-2 border-green-600"
-                : "bg-red-100 text-red-800 border-2 border-red-600"
-            }`}
-          >
-            {deptoObjetoPrueba.estado}
-          </span>
-          <div>
-            <Lock />
-            <LockOpen />
+        <div className="flex gap-24 mr-6">
+          <div className="flex items-center">
+            <span
+              className={`px-5 py-3 rounded-full text-xl font-semibold ${
+                deptoObjetoPrueba.estado === "Ocupado"
+                  ? "bg-green-100 text-green-800 border-2 border-green-600"
+                  : "bg-red-100 text-red-800 border-2 border-red-600"
+              }`}
+            >
+              {deptoObjetoPrueba.estado}
+            </span>
           </div>
+          <button onClick={() => setEdit(!edit)} className="flex items-center border border-gray-200 shadow-md rounded-full h-20 w-20 justify-center hover:shadow-lg transition-all cursor-pointer">
+            {!edit ? <Lock className="w-10 h-10"/> : <LockOpen className="w-10 h-10"/>}
+          </button>
         </div>
       </div>
       <div className="container mx-auto w-full mr-14 px-0 mt-10">
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 ml-3 items-start justify-items-stretch min-w-full text-lg ">
           <div className="min-w-56">
-            <label htmlFor="ubicacion" className="font-bold"> Ubicación </label>
+            <label htmlFor="ubicacion" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Ubicación </label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder="Ej: San Juan 382 piso 7"
               name="ubicacion"
               value={deptoObjetoPrueba.ubicacion}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="propietario" className="font-bold"> Propietario </label>
+            <label htmlFor="propietario" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Propietario </label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder="Ej: Lionel Messi"
               name="propietario"
               value={deptoObjetoPrueba.propietario}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="usufructuario" className="font-bold"> Usufructuario </label>
+            <label htmlFor="usufructuario" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Usufructuario </label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder="Ej: Lionel Messi"
               name="usufructuario"
               value={deptoObjetoPrueba.usufructuario}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="locador" className="font-bold"> Locador </label>
+            <label htmlFor="locador" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Locador </label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder="Ej: Angel Di Maria"
               name="locador"
               value={deptoObjetoPrueba.locador}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="locatario" className="font-bold"> Locatario / Inquilino{" "} </label>
+            <label htmlFor="locatario" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Locatario / Inquilino{" "} </label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder="Ej: Mateo Messi"
               name="locatario"
               value={deptoObjetoPrueba.locatario}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="facturador" className="font-bold"> Facturador </label>
+            <label htmlFor="facturador" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Facturador </label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder="Ej: Angel Di Maria"
               name="facturador"
               value={deptoObjetoPrueba.facturador}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="descripcion" className="font-bold"> Descripción </label>
+            <label htmlFor="descripcion" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Descripción </label>
             <Textarea
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder="Ej: 5 dormitorios, 3 baños, 350mt², edificio con pileta y parrilla en la terraza."
               name="descripcion"
               value={deptoObjetoPrueba.descripcion}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="vencimientoUsufructo" className="font-bold"> Vencimiento del Usufructo </label>
+            <label htmlFor="vencimientoUsufructo" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Vencimiento del Usufructo </label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               type="date"
               name="vencimientoUsufructo"
               value={deptoObjetoPrueba.vencimientoUsufructo}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="Contrato" className="font-bold"> Contrato </label>
+            <label htmlFor="Contrato" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Contrato </label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder="Ej: "
               type="file"
               name="Contrato"
               value={deptoObjetoPrueba.contrato}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="precioInicio" className="font-bold"> Precio al inicio </label>
+            <label htmlFor="precioInicio" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}> Precio al inicio </label>
             <div className="relative">
               <span className="absolute top-[6px] left-[8px] font-bold"> $ </span>
               <Input
-                className="mt-2 text-md p-2 pl-6"
+              className={`mt-2 text-xl ${edit ? 'text-gray-600 pl-6' : 'text-black border-none pl-6'}`}
                 placeholder="160.000,00"
                 type="number"
                 name="precioInicio"
                 value={deptoObjetoPrueba.precioInicio}
-                readOnly="true"
+                readOnly={!edit}
               />
             </div>
           </div>
           <div className="min-w-56">
-            <label htmlFor="precioActual" className="font-bold">Precio Actual</label>
+            <label htmlFor="precioActual" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}>Precio Actual</label>
             <div className="relative">
             <span className="absolute top-[6px] left-[8px] font-bold"> $ </span>
               <Input
-                className="mt-2 text-md p-2 pl-6"
-                placeholder="360.000,00"
+              className={`mt-2 text-xl ${edit ? 'text-gray-600 pl-6' : 'text-black border-none pl-6'}`}
+              placeholder="360.000,00"
                 type="number"
                 name="precioActual"
                 value={deptoObjetoPrueba.precioActual}
-                readOnly="true"
+                readOnly={!edit}
               />
             </div>
           </div>
           <div className="min-w-56">
-            <label htmlFor="ultimaActualizacionPrecio" className="font-bold">Ultima actualización del precio</label>
+            <label htmlFor="ultimaActualizacionPrecio" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}>Ultima actualización del precio</label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder=""
               type="date"
               name="ultimaActualizacionPrecio"
               value={deptoObjetoPrueba.ultimaActualizacionPrecio}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56 flex items-center gap-5 mt-[44px]">
-            <label htmlFor="reli" className="font-bold">Inscripto en RELI</label>
+            <label htmlFor="reli" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}>Inscripto en RELI</label>
             <Checkbox
               className="h-6 w-6"
               name="reli"
               value={deptoObjetoPrueba.reli}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="documentosVarios" className="font-bold">Documentos varios</label>
+            <label htmlFor="documentosVarios" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}>Documentos varios</label>
             <Input
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder=""
               type="text"
               name="documentosVarios"
               value={deptoObjetoPrueba.documentosVarios}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
           <div className="min-w-56">
-            <label htmlFor="obsDatos" className="font-bold">{" "}Observaciónes / datos extras</label>
+            <label htmlFor="obsDatos" className={`font-bold text-md ${!edit ? 'text-[#dbdbdb] p-2' : 'text-black border-none p-2'}`}>{" "}Observaciónes / datos extras</label>
             <Textarea
-              className="mt-2 text-md p-2"
+              className={`mt-2 text-xl  ${edit ? 'text-gray-600 p-2' : 'text-black border-none p-2'}`}
               placeholder="Ej: Admite mascotas, fue reaconcidionado recientemente, cocina nueva a estrenar."
               name="obsDatos"
               value={deptoObjetoPrueba.obsDatos}
-              readOnly="true"
+              readOnly={!edit}
             />
           </div>
 
