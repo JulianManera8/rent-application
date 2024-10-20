910447ac788b3bbc6c7f945ea960a462c77c5b1f
JulianManera8 <julianmanera@hotmail.com>
2024-10-20 19:57:44 -0300

    improved responsiveness

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -14,8 +14,8 @@ export const meta = () => {
 export default function Index() {
   return (
     <div className="font-sans flex md:flex-row flex-col items-center justify-evenly bg-slate-100 min-h-screen">
-      <section className="md:w-1/2 w-full h-[30vh] md:h-screen flex items-center justify-center relative">
-        <div className="absolute top-[10%] md:top-[15%] flex flex-col text-center items-center space-y-7">
+      <section className="md:w-1/2 w-full md:h-screen mt-8 flex items-center justify-center relative">
+        <div className="md:absolute top-[10%] md:top-[15%] flex flex-col text-center items-center space-y-7">
           <img
             src="/logo.png"
             alt="Logo"
@@ -27,7 +27,7 @@ export default function Index() {
       </section>
 
       <section className="flex flex-1 w-full md:w-[600px] h-[50vh] md:h-screen items-center justify-center relative">
-        <div className="flex flex-col items-start gap-10 mx-auto w-[90%] md:w-[65%] absolute top-[10%] md:top-[15%]">
+        <div className="flex flex-col items-start gap-10 mx-auto w-[90%] md:w-[65%] absolute top-[7%] md:top-[15%]">
           <Tabs defaultValue="account" className="w-full shadow-2xl bg-transparent border border-gray-300 rounded-md">
             <TabsList className="grid grid-cols-2 w-full h-16 md:h-20 bg-[#003156]">
               <TabsTrigger value="account" className="h-[75%] text-lg md:text-xl w-[95%] mx-auto font-bold text-gray-300">
