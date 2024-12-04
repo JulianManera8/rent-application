b76fdb938dba515b9fc9691a5378527567e6d3ba
JulianManera8 <julianmanera@hotmail.com>
2024-12-04 20:32:56 -0300

    fix

--- app/components/dashboardGeneal/groupSection/GroupQuantity.jsx
+++ app/components/dashboardGeneal/groupSection/GroupQuantity.jsx
@@ -36,8 +36,8 @@ export default function GroupQuantity({ userId }) {
     }, [loading])
 
     return (
-        <Card className="border-1 shadow-md shadow-black/15 border-[0.6px] border-t-[1px] min-h-[100px] h-full flex flex-col justify-between">
-            <CardHeader className="flex flex-row items-center justify-start w-full space-y-0 py-2">
+        <Card className="border-1 shadow-md shadow-black/15 border-[0.6px] border-t-[1px] min-h-[100px] h-full flex flex-col justify-between pb-2">
+            <CardHeader className="flex flex-row items-center justify-start w-full space-y-0 pt-2 pb-0">
                 <CardTitle className="text-lg font-medium text-left w-full">
                     Grupos Creados
                 </CardTitle>
@@ -46,7 +46,7 @@ export default function GroupQuantity({ userId }) {
             <CardContent className="flex flex-col justify-between">
                 {loading || showSkeleton ? (
                     <>
-                        <span className="text-lg text-zinc-200 font-normal mb-2"> <Skeleton className="w-28 h-4 bg-zinc-200"/> </span> 
+                        <span className="text-lg text-zinc-200 font-normal mb-2 -mt-3"> <Skeleton className="w-28 h-4 bg-zinc-200"/> </span> 
                         <span className="text-sm text-zinc-200"> <Skeleton className="w-28 h-4 bg-zinc-200"/> </span>
                     </>
                 ) : (
